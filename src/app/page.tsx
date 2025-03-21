868315b08656e9a87f8464ca30ad32ae53e7c280
⸸ <102256366+yuliglocky@users.noreply.github.com>
2025-03-20 20:44:40 -0500

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { Metadata } from "next";
-import CTA from "@/app/(site)/Cta";
-import FAQ from "@/app/(site)/Faq";
-import FeaturedTime from "@/app/(site)/FeaturedTime";
-import Footer from "@/app/(site)/Footer";
-import HeroSection from "@/app/(site)/Hero";
-import MakerIntro from "@/app/(site)/MakerIntro";
-import Navbar from "@/app/(site)/Navbar";
-import PricingSection from "@/app/(site)/pricing";
-import TestimonialsPage from "@/app/(site)/Testimonials";
+
 
 // required by Nextra
 export const metadata: Metadata = {
@@ -17,15 +9,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="bg-[#212121]">
-      <Navbar />
-      <HeroSection />
-      <FeaturedTime />
-      <MakerIntro />
-      <PricingSection />
-      <FAQ />
-      <TestimonialsPage />
-      <CTA />
-      <Footer />
+    
     </div>
   );
 }
